a57490e2d903acef01af3aa6bc253bc22f3b7662
Jenn <cookiimii@live.nl>
2019-06-01 21:39:09 +0200

    Got contacts in the friends tab, working together with fragments !!

--- app/src/main/java/com/example/festivalapptest/FriendsFragment.java
+++ app/src/main/java/com/example/festivalapptest/FriendsFragment.java
@@ -1,25 +1,31 @@
 package com.example.festivalapptest;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+        import android.Manifest;
+        import android.content.pm.PackageManager;
+        import android.database.Cursor;
+        import android.os.Bundle;
+        import android.provider.ContactsContract;
+        import android.support.annotation.NonNull;
+        import android.support.annotation.Nullable;
+        import android.support.v4.app.ActivityCompat;
+        import android.support.v4.app.Fragment;
+        import android.support.v4.content.ContextCompat;
+        import android.util.Log;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ArrayAdapter;
+        import android.widget.ListView;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-import java.util.ArrayList;
+        import java.util.ArrayList;
+
+        import static android.content.ContentValues.TAG;
 
 public class FriendsFragment extends Fragment {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private static final int PERMISSION_REQUEST_READ_CONTACTS = 100;
     Cursor cursor;
     ArrayList<String> contacts;
@@ -47,15 +53,47 @@ public class FriendsFragment extends Fragment {
             showContacts();
         } else {
             // Using getActivity because the activity doesn't need to be declared in a fragment
-            ActivityCompat.requestPermissions(getActivity(),
+            ActivityCompat.requestPermissions(
+                    getActivity(),
                     new String[] { Manifest.permission.READ_CONTACTS } ,
                     PERMISSION_REQUEST_READ_CONTACTS );
         }
 
-        /////////////////////////
-        // Resource is single listitem !!!!
-        /////////////////////////
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),R.id., contacts );
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getContext(),R.layout.friend_item, contacts );
+
+        // Only works when there are contacts in the list
+        // Maybe need to add an exception later on 
+        listView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        if (requestCode == PERMISSION_REQUEST_READ_CONTACTS){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                showContacts();
+            }
+        } else {
+            Toast.makeText(getContext(), "Please give permission to show the list of contacts", Toast.LENGTH_LONG);
+        }
+    }
+
+    private void showContacts(){
+        cursor = getContext().getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                null, null, null, ContactsContract.Contacts.DISPLAY_NAME + " ASC");
+
+        contacts = new ArrayList<>();
+
+        while(cursor.moveToNext()){
+            String contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            contacts.add("Name: " + contactName + "\n" + "Phone Number: " + phoneNumber );
+
+            Log.d(TAG,"showContacts: " + contacts);
+        }
+
+        cursor.close();
     }
 }
